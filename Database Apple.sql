6cd65317bbb17a45e086bc48c8185a9908894bde
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2022-10-17 19:31:43 +0800

    edited database apple and models.py

--- Database Apple.sql
+++ Database Apple.sql
@@ -48,6 +48,7 @@ DROP TABLE IF EXISTS `job_role`;
 CREATE TABLE IF NOT EXISTS `job_role` (
   `JobRole_ID` int NOT NULL AUTO_INCREMENT,
   `JobRole_Name` varchar(20) NOT NULL,
+  `Department` varchar(50) NOT NULL,
   `JobRole_Desc` varchar(255) NOT NULL,
   PRIMARY KEY (`JobRole_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
