cc9885c5a7d3feb9c9757c9ad591b67734d76c20
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2022-10-29 20:09:17 +0800

    added fixture. Same as setup and teardown methods

--- spm_project/src/Flask/testing/functional_testing/test_course_route.py
+++ spm_project/src/Flask/testing/functional_testing/test_course_route.py
@@ -3,11 +3,10 @@ from ljapi.routes.courseroute import viewAllCourse
 from ljapi.application import create_app
 from flask_cors import CORS
 import json
-def test_course_view_all_route():
-    app = create_app()
-    print(app)
+def test_course_view_all_route(test_client):
+    # app = create_app()
+    # print(app)
     # Creating a test client based on the Flask application
-    with app.test_client() as test_client:
         response = test_client.get("http://127.0.0.1:5000/course/view/all")
         data = json.loads(response.get_data(as_text=True))
         print(data)
--- spm_project/src/Flask/testing/unit_testing/test_skill_model.py
+++ spm_project/src/Flask/testing/unit_testing/test_skill_model.py
@@ -5,26 +5,29 @@ import json
 # In each model, we will test the following:
 # 1. whether the object is being initialised properly
 # 2. Whether the methods are to_dict is return successfully
-def test_skill_intialisation():
-    # GIVEN: Skill is initialised
-    # WHEN: Skill object is being populated
+def test_skill_intialisation(new_skill):
+    """
+    GIVEN a SKILL model
+    WHEN a new Skill is being created
+    THEN check whether the skill_id, skill_name, skill_desc and skill_status are defined correctly
+    """
     try:
-        skill = Skill(skill_id=100,skill_name="testing_skill",skill_desc="Testing desc",skill_status="Active")
-    # Then:
     # Check whether all the fields are correct
-        assert skill.skill_id == 100
-        assert skill.skill_name == "testing_skill"
-        assert skill.skill_desc == "Testing desc"
-        assert skill.skill_status == "Active"
+        assert new_skill.skill_id == 100
+        assert new_skill.skill_name == "testing_skill"
+        assert new_skill.skill_desc == "Testing desc"
+        assert new_skill.skill_status == "Active"
     except:
         raise Exception("Test case of test_skill_intialisation failed")
 
-def test_skill_method():
-    # GIVEN: Skill is intialised
-    skill = Skill(skill_id=100,skill_name="testing_skill",skill_desc="Testing desc",skill_status="Active")
-    # WHEN: to_dict method is being called
+def test_skill_method(new_skill):
+    """
+    GIVEN a SKILL model
+    WHEN a new Skill is being created
+    THEN check whether the methods are executed correctly
+    """
     try: 
-        convertedDictionary = skill.to_dict()
+        convertedDictionary = new_skill.to_dict()
         print(convertedDictionary)
         # THEN: Check for the following conditions
         assert type(convertedDictionary) == dict
