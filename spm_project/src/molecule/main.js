265d9e170f0a0336330ac5ee5b315d6ed33aeaf4
WeiTingsys <lauweiting2012@gmail.com>
2022-11-01 12:23:18 +0800

    View all learning journey landing page

--- spm_project/src/molecule/main.js
+++ spm_project/src/molecule/main.js
@@ -24,6 +24,7 @@ import { CardActionArea } from '@mui/material';
 
 
 function Main() {
+    // Weiting done this
     const navigate = useNavigate();
 
     const [output, setoutput] = useState([])
